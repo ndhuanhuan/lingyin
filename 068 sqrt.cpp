aab5f170bae2aedfdf94b0ab7e7999e92f6f250a
ndhuanhuan <ndhuanhuan@gmail.com>
2016-12-16 21:54:28 -0600

    Update 068 sqrt.cpp

--- 068 sqrt.cpp
+++ 068 sqrt.cpp
@@ -1,4 +1,12 @@
 double sqrt(double x)
 {
   if(x==0) return 0;
+  double last = 0.0;
+  double res =1.0;
+  while(abs(res-last)>0.0001)
+  {
+    last = res;
+    res =(res+x/res)/2;
+  }
+  return res;
 }
